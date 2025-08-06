fb15c0740dadc08bd087ef9e0463317691ab850b
CalebUnah7 <kcayala@unah.hn>
2025-08-06 00:57:32 -0600

    cambios realizados en la base de datos

--- init/init.sql
+++ init/init.sql
@@ -2,20 +2,34 @@ CREATE TABLE publicaciones (
     id char(36) primary key,
     titulo varchar(255) not null,
     contenido text not null,
-    autor varchar (100) not null,
-    fecha_creacion timestamp default current_timestamp
+    autorID BINARY(16) not null,
+    fecha_creacion timestamp default current_timestamp,
+    activo boolean default true,
+    publica boolean default true,
+    FOREIGN KEY (autorID) REFERENCES users(id)
 );
 CREATE TABLE comentariosPublicaciones (
     id char(36) primary key,
     publicacion_id char(36) NOT NULL,
-    user_id INT,
+    user_id BINARY(16) NOT NULL,
     comentario TEXT,
     activo BOOLEAN DEFAULT TRUE,    
     fecha_creacion TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (publicacion_id) REFERENCES publicaciones(id)
-    
+    FOREIGN KEY (publicacion_id) REFERENCES publicaciones(id),
+    FOREIGN KEY (user_id) REFERENCES users(id)
 );
-Insert into publicaciones (id, titulo, contenido, autor) values 
+
+CREATE TABLE users (
+  id BINARY(16) PRIMARY KEY,
+  email VARCHAR(255) UNIQUE NOT NULL,
+  nombre VARCHAR(100) NOT NULL,
+  password_hash VARCHAR(255) NOT NULL,
+  must_change_password BOOLEAN DEFAULT TRUE,
+  role ENUM('user', 'admin') DEFAULT 'user',
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+Insert into publicaciones (id, titulo, contenido, autorID) values 
 (UUID(), 'Como utilizar tu tarjeta de credito de manera inteligente', 'Analiza bien tus compras y los gastos necesarios de cada mes...', 'Abrahan'),
 (UUID(), 'Reseña de productos de belleza en tendencia', 'Comparamos precio-calidad, hay otro productos que se le parecen..', 'BeautyBlogger'),
 (UUID(), 'Países que debes de visitar si o si', 'Te muestro lugares y tips de viaje...', 'Un_ViajeroxElMundo');
--- models/comentario.model.js
+++ models/comentario.model.js
@@ -1,6 +1,8 @@
 import pool from '../config/db.js'
 
 export async function CreateComentario(id,publicacion_id,user_id,comentario){
+    //TODO: cambiamos la tabla comentariosPublicaciones realizar sus respectivos
+    //TODO: cambios en el controlador
     const query = `INSERT INTO comentariosPublicaciones (id,publicacion_id,user_id,comentario)
                     VALUES (?,?,?,?)`
     const [rows] = await pool.query(query,[id,publicacion_id,user_id,comentario])
